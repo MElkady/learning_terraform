c987e46573e5c34ec798c28a0fc9e41fc2c60761
Mohamed Elkady <mohamedelkady@gmail.com>
2022-07-20 22:42:01 +0200

    Adding Frankfurt VPC

--- main.tf
+++ main.tf
@@ -131,6 +131,17 @@ resource "aws_s3_bucket" "fra_bucket" {
   provider      = aws.fra
 }
 
+module "fra_vpc" {
+  providers = {
+    aws = aws.fra
+  }
+  source         = "terraform-aws-modules/vpc/aws"
+  name           = "my-vpc"
+  cidr           = "10.0.0.0/16"
+  azs            = ["eu-central-1a"]
+  public_subnets = ["10.0.1.0/24"]
+}
+
 output "fra_bucket_arn" {
   value = aws_s3_bucket.fra_bucket.arn
 }
